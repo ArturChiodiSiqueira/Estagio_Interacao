3a8287d8e95ba36fadee2e82d1186be84c3fb2a0
artur <artur@DESKTOP-V0JU5PP>
2022-08-15 11:42:42 -0300

    save

---
+++ Tabuada/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Tabuada
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int tabuada, inicio, parada, resultado;
+
+            Console.Write("Informe uma tabuada para ser calculada: ");
+            tabuada = int.Parse(Console.ReadLine());
+
+            Console.Write("\nInforme o número de inicio da tabuada: ");
+            inicio = int.Parse(Console.ReadLine());
+
+            Console.Write("Informe o número de parada da tabuada: ");
+            parada = int.Parse(Console.ReadLine());
+
+            for (int i = inicio; i <= parada; i++)
+            {
+                resultado = i * tabuada;
+                Console.WriteLine("\n" + i + " x " + tabuada + " = " + resultado);
+            }
+        }
+    }
+}
