dc75f3e7deaa15c54b0fc7b75e7441b5641fd7f0
artur <artur@DESKTOP-V0JU5PP>
2022-08-15 10:39:03 -0300

    save

---
+++ SimuladorCalculadora/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace SimuladorCalculadora
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            float numero1, numero2, resultado;
+
+            Console.Write("Informe o 1° valor: ");
+            numero1 = float.Parse(Console.ReadLine());
+
+            Console.Write("Informe o 2° valor: ");
+            numero2 = float.Parse(Console.ReadLine());
+        }
+    }
+}
