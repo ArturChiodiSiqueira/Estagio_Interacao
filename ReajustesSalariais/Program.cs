5b6d446460a8c89c5aa36a43c112dca73d6f78b1
artur <artur@DESKTOP-V0JU5PP>
2022-08-15 19:14:23 -0300

    save

---
+++ ReajustesSalariais/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace ReajustesSalariais
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double salarioAtual, salarioFinal;
+
+            do
+            {
+                Console.Clear();
+                Console.Write("Informe seu salario atual: ");
+                salarioAtual = double.Parse(Console.ReadLine());
+            } while (salarioAtual < 1000);
+
+            if (salarioAtual < 2000)
+            {
+                salarioFinal = (salarioAtual * 0.1) + salarioAtual;
+                Console.WriteLine("O salario final é: " + salarioFinal);
+            }
+            if (salarioAtual >= 2000 && salarioAtual < 3000)
+            {
+                salarioFinal = (salarioAtual * 0.05) + salarioAtual;
+                Console.WriteLine("O salario final é: " + salarioFinal);
+            }
+            if (salarioAtual >= 3000)
+            {
+                salarioFinal = salarioAtual;
+                Console.WriteLine("Sem aumento para você! :(");
+            }
+            Console.WriteLine("\n\nPressione qualquer tecla para finalizar.");
+            Console.ReadKey();
+        }
+    }
+}
