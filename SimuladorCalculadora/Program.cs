b49580160305727c1500a42416c3cf9f3effa132
artur <artur@DESKTOP-V0JU5PP>
2022-08-15 10:58:02 -0300

    save

--- SimuladorCalculadora/Program.cs
+++ SimuladorCalculadora/Program.cs
@@ -6,7 +6,7 @@ namespace SimuladorCalculadora
     {
         static void Main(string[] args)
         {
-            float numero1, numero2, resultado;
+            float numero1, numero2, resultado=0;
             int opcao;
 
             Console.Write("Informe o 1° valor: ");
@@ -18,23 +18,34 @@ namespace SimuladorCalculadora
             Console.Write("Informe a operação desejada:\n\t{1} Soma\n\t{2} Subtração\n\t{3} Multiplicação\n\t{4} Divisão\nOpção: ");
             opcao = int.Parse(Console.ReadLine());
 
-            if (opcao == 1)
+            if (opcao < 1 || opcao > 4)
             {
-                resultado = numero1 + numero2;
+                Console.WriteLine("Informe uma opção valida!");
             }
-            if (opcao == 2)
+            else
             {
-                resultado = numero1 - numero2;
+                if (opcao == 1)
+                {
+                    resultado = numero1 + numero2;
+                }
+                if (opcao == 2)
+                {
+                    resultado = numero1 - numero2;
+                }
+                if (opcao == 3)
+                {
+                    resultado = numero1 * numero2;
+                }
+                if (opcao == 4)
+                {
+                    if (numero2 == 0)
+                    {
+                        Console.WriteLine("Não é possivel dividir por zero!");
+                    }
+                    else
+                    resultado = numero1 / numero2;
+                }
             }
-            if (opcao == 3)
-            {
-                resultado = numero1 * numero2;
-            }
-            if (opcao == 4)
-            {
-                resultado = numero1 / numero2;
-            }
-
             Console.WriteLine("O resultado da operação é: " + resultado);
         }
     }
