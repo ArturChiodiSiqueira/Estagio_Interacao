2bedc5b7825e859703401a774a8c62eedf3c61fe
artur <artur@DESKTOP-V0JU5PP>
2022-08-15 10:46:50 -0300

    save

--- SimuladorCalculadora/Program.cs
+++ SimuladorCalculadora/Program.cs
@@ -7,12 +7,23 @@ namespace SimuladorCalculadora
         static void Main(string[] args)
         {
             float numero1, numero2, resultado;
+            int opcao;
 
             Console.Write("Informe o 1° valor: ");
             numero1 = float.Parse(Console.ReadLine());
 
             Console.Write("Informe o 2° valor: ");
             numero2 = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("Informe a operação desejada:\n\t{1} Soma\n\t{2} Subtração\n\t{3} Multiplicação\n\t{4} Divisão\nOpção: ");
+            opcao = int.Parse(Console.ReadLine());
+
+            if (opcao == 1)
+            {
+                resultado = numero1 + numero2;
+            }
+
+            Console.WriteLine("O resultado da operação é: " + resultado);
         }
     }
 }
