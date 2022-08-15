9469618653f9d6faca5f7b565fa9b19226282030
artur <artur@DESKTOP-V0JU5PP>
2022-08-15 19:07:24 -0300

    save

---
+++ ConversaoDeGraus/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ConversaoDeGraus
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double grauC, grauF;
+
+            Console.Write("Informe a temperatura em °C: ");
+            grauC = double.Parse(Console.ReadLine());
+
+            grauF = grauC * 1.8 + 32;
+
+            Console.WriteLine("A temperatura em °F é: " + grauF);
+
+            Console.WriteLine("\n\nPressione qualquer tecla para finalizar.");
+            Console.ReadKey();
+        }
+    }
+}
