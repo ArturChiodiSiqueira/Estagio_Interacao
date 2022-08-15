caf54f1e1c3e7341a751c7855d7bb079877922b6
artur <artur@DESKTOP-V0JU5PP>
2022-08-15 19:22:12 -0300

    save

---
+++ PrimoOuNao/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace PrimoOuNao
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double lado1, lado2, lado3;
+
+            Console.Write("informe o primeiro lado: ");
+            lado1 = double.Parse(Console.ReadLine());
+
+            Console.Write("informe o segundo lado: ");
+            lado2 = double.Parse(Console.ReadLine());
+
+            Console.Write("informe o terceiro lado: ");
+            lado3 = double.Parse(Console.ReadLine());
+
+            if (lado1 == lado2 && lado2 == lado3)
+            {
+                Console.WriteLine("\nTriângulo Equilatero.");
+            }
+            else if (lado1 == lado2 || lado2 == lado3 || lado1 == lado3)
+            {
+                Console.WriteLine("\nTriângulo Isosceles.");
+            }
+            else
+            {
+                Console.WriteLine("\nTriângulo Escaleno");
+            }
+            Console.WriteLine("\n\nPressione qualquer tecla para finalizar.");
+            Console.ReadKey();
+        }
+    }
+}
