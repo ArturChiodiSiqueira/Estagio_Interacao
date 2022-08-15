ea05a0b31d533c60700a86e369bd704c66b63afc
artur <artur@DESKTOP-V0JU5PP>
2022-08-15 15:29:19 -0300

    save

---
+++ MediaAluno/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MediaAluno
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
