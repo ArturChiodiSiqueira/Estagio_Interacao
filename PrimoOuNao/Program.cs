c2f7e1e11c4f0e6477e7257d7b78deb221f5c87b
artur <artur@DESKTOP-V0JU5PP>
2022-08-16 10:03:00 -0300

    save

--- PrimoOuNao/Program.cs
+++ PrimoOuNao/Program.cs
@@ -6,18 +6,27 @@ namespace PrimoOuNao
     {
         static void Main(string[] args)
         {
-            int numero;
+            int num, resultado = 0, aux;
 
             Console.Write("Informe um número: ");
-            numero = int.Parse(Console.ReadLine());
+            num = int.Parse(Console.ReadLine());
 
-            if (numero % 2 != 0 || numero == 2)
+            for (aux = num; aux >= 1; aux--)
             {
-                Console.WriteLine("O número é primo!");
+                Console.WriteLine(num + " / " + aux);
+                if (num % aux == 0)
+                {
+                    resultado++;
+                }
+                Console.WriteLine(resultado);
+            }
+            if (resultado != 2)
+            {
+                Console.WriteLine("O número não é primo!");
             }
             else
             {
-                Console.WriteLine("O numero não é primo");
+                Console.WriteLine("O numero é primo!");
             }
             Console.WriteLine("\n\nPressione qualquer tecla para finalizar.");
             Console.ReadKey();
