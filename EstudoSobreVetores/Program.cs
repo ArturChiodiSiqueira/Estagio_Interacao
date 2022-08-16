0db257354992d701ac2a8220c38ccce7fb55ca66
artur <artur@DESKTOP-V0JU5PP>
2022-08-16 15:09:08 -0300

    save

--- EstudoSobreVetores/Program.cs
+++ EstudoSobreVetores/Program.cs
@@ -6,7 +6,34 @@ namespace EstudoSobreVetores
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int[] vet = new int[4];
+            int i, valor;
+
+            Console.WriteLine("Informe os valores para o vetor: ");
+            for (i = 0; i < 4; i++)
+            {
+                Console.Write("informe o " + (i + 1) + "° valor: ");
+                vet[i] = int.Parse(Console.ReadLine());
+            }
+            Console.WriteLine("\nos valores informados são: ");
+            for (i = 0; i < 4; i++)
+            {
+                Console.WriteLine("vet [" + i + "] = " + vet[i]);
+            }
+
+            Console.WriteLine("Informe um valor para multiplicar todos os valores do vetor: ");
+            valor = int.Parse(Console.ReadLine());
+
+            for (i = 0; i < 4; i++)
+            {
+                vet[i] = vet[i] * valor;
+            }
+
+            Console.WriteLine("\nos valores resultantes são: ");
+            for (i = 0; i < 4; i++)
+            {
+                Console.WriteLine("vet [" + i + "] = " + vet[i]);
+            }
         }
     }
 }
